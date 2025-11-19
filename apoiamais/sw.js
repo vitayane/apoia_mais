8697aa832238444db512a78a238ba5f4711c8a02
Thammy Silva <thamirysmatos@yahoo.com.br>
2025-11-19 20:06:51 -0300

    Delete apoiamais directory

--- apoiamais/sw.js
+++
@@ -1,8 +0,0 @@
-// Service Worker básico
-self.addEventListener('install', function(event) {
-    console.log('Service Worker instalado');
-});
-
-self.addEventListener('fetch', function(event) {
-    // Aqui você pode implementar cache se quiser offline
-});
